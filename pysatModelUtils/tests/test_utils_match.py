f41e04decc560175920ebcbd46b4ae80021b7c9a
Angeline Burrell <angeline.burrell@nrl.navy.mil>
2019-10-04 11:57:12 -0400

    TST: parametrized input failures
    Parametrized the input failure tests, increasing coverage and reducing duplicate code.

--- pysatModelUtils/tests/test_utils_match.py
+++ pysatModelUtils/tests/test_utils_match.py
@@ -1,44 +1,63 @@
+# Author: Angeline Burrell, NRL, 2019
+
 from __future__ import absolute_import, unicode_literals
 
-from pytest import raises
+from datetime 
+import pytest
 
 import pysat
 
+import pysatModelUtils as pysat_mu
 import pysatModelUtils.utils.match as match
 
 
-class TestUtilsMatch():
+class TestUtilsMatchCollectInstModPairs():
+    """ Unit tests for utils.match.collect_inst_model_pairs """
     def setup(self):
         """Runs before every method to create a clean testing setup."""
-        self.testInst = pysat.Instrument(platform=str('pysat'),
-                                         name=str('testing'),
-                                         clean_level='clean')
-        self.start = pysat.datetime(2009, 1, 1)
-        self.stop = pysat.datetime(2009, 1, 1)
+        self.input_args = {"start": dt.datetime(2009, 1, 1),
+                           "stop": dt.datetime(2009, 1, 1),
+                           "tinc": dt.timedelta(days=1),
+                           "inst": pysat.Instrument(platform=str('pysat'),
+                                                    name=str('testing'),
+                                                    clean_level='clean')}
+        self.required_kwargs = {"model_load_kwargs":
+                                {"model_inst":
+                                 pysat.Instrument(platform=str('pysat'),
+                                                  name=str('testing'),
+                                                  clean_level='clean')},
+                                "inst_lon_name": "longitude",
+                                "mod_lon_name": "longitude",
+                                "inst_name": ["longitude", "latitude", "slt"],
+                                "mod_name": ["longitude", "latitude", "slt"],
+                                "mod_datetime_name": "Epoch",
+                                "mod_time_name": "uts",
+                                "mod_units": ["deg", "deg", "h"]}
+        self.start = 
+        self.stop = 
 
     def teardown(self):
         """Runs after every method to clean up previous testing."""
-        del self.testInst, self.start, self.stop
+        del self.input_args, self.required_kwargs
 
-    def test_collect_inst_model_pairs_wo_date(self):
-        """Try to run without start or stop dates"""
-        with raises(ValueError) as verr:
-            match.collect_inst_model_pairs(inst=self.testInst)
+    @pytest.mark.parametrize("del_key,err_msg",
+                             [("model_load_kwargs",
+                               "must provide a pysat.Instrument object"),
+                              ("inst_lon_name", "Need longitude name for inst"),
+                              ("mod_lon_name", "Need longitude name for model"),
+                              ("inst_name", "Must provide instrument location"),
+                              ("mod_name", "Must provide the same number"),
+                              ("mod_units", "Must provide units for each "),
+                              ("mod_datetime_name", "Need datetime coordinate"),
+                              ("mod_time_name", "Need time coordinate")])
+    def test_input_failure(self, del_key, err_msg):
+        """ Test for expected failure when missing necessary input from kwargs
+        """
+        del self.required_kwargs[del_key]
 
-        assert verr.value.args[0].find('Must provide start and end time') >= 0
+        with pytest.raises(ValueError) as verr:
+            match.collect_inst_model_pairs(**self.input_args,
+                                           **self.required_kwargs)
 
-    def test_collect_inst_model_pairs_wo_inst(self):
-        """Try to run without an instrument"""
-        with raises(ValueError) as verr:
-            match.collect_inst_model_pairs(start=self.start, stop=self.stop)
-
-        assert verr.value.args[0].find('Must provide a pysat instrument') >= 0
-
-    def test_collect_inst_model_pairs_wo_model(self):
-        """Try to run without a model"""
-        with raises(ValueError) as verr:
-            match.collect_inst_model_pairs(start=self.start, stop=self.stop,
-                                           inst=self.testInst)
-
-        assert(verr.value.args[0].find('Must provide list of modelled data')
-               >= 0)
+        assert verr.value.args[0].find(err_msg) >= 0
+    
