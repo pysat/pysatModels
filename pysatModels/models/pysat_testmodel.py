0ce9e4cee1b0cc9d8394f8a7ed58db4cf2b20cfd
Jeff Klenzing <jklenzing@gmail.com>
2020-02-01 14:35:00 -0500

    ENH: 15 minute resolution in test model

--- pysatModels/models/pysat_testmodel.py
+++ pysatModels/models/pysat_testmodel.py
@@ -55,7 +55,7 @@ def load(fnames, tag=None, sat_id=None):
 
     # Create one day of data at desired frequency
     index = pds.date_range(start=date, end=date+pds.DateOffset(seconds=86399),
-                           freq='9000s')
+                           freq='900s')
 
     uts = index.hour*3600 + index.minute*60 + index.second
 
