95102e2dff182a568292e2c25753702e3433a69c
Jeff Klenzing <jklenzing@gmail.com>
2020-04-07 12:38:27 -0400

    STY: remove imports

--- pysatModels/tests/test_models.py
+++ pysatModels/tests/test_models.py
@@ -2,8 +2,6 @@ import pytest
 
 import pysatModels
 
-import pysat.tests
-import pysat.tests.test_instruments
 from pysat.tests.test_instruments import generate_instrument_list
 from pysat.tests.test_instruments import TestInstrumentsAll
 from pysat.tests.test_instruments import TestInstrumentsDownload
