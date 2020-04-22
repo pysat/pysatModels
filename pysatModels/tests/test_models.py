8c77bb8b76ecc7ef51cfaeb1838ba74a6c91f10a
Jeff Klenzing <jklenzing@gmail.com>
2020-04-21 20:25:04 -0400

    TST: tidy up model tests

--- pysatModels/tests/test_models.py
+++ pysatModels/tests/test_models.py
@@ -1,75 +1,34 @@
 import pytest
 
 import pysatModels
-
-from pysat.tests.test_instruments import generate_instrument_list
-from pysat.tests.test_instruments import TestInstrumentsAll
-from pysat.tests.test_instruments import TestInstrumentsDownload
-from pysat.tests.test_instruments import TestInstrumentsNoDownload
+from pysat.tests.instrument_tests import generate_instrument_list
+from pysat.tests.instrument_tests import InstTestClass
 
 instruments = generate_instrument_list(pysatModels.models.__all__,
                                        package='pysatModels.models')
 
-InstClasses = [TestInstrumentsAll, TestInstrumentsDownload,
-               TestInstrumentsNoDownload]
-
-for InstClass in InstClasses:
-    # Need to turn off __test__ so that tests are not run until modified
-    InstClass.__test__ = False
-    method_list = [func for func in dir(InstClass)
-                   if callable(getattr(InstClass, func))]
-    # Search tests for iteration via pytestmark, update instrument list
-    for method in method_list:
-        if hasattr(getattr(InstClass, method), 'pytestmark'):
-            Nargs =  len(getattr(InstClass, method).pytestmark)
-            names = [getattr(InstClass, method).pytestmark[j].name
-                     for j in range(0, Nargs)]
-            # Remove instruments imported from pysat
-            getattr(InstClass, method).pytestmark.clear()
-            # Add instruments from pysatModels
-            if InstClass == TestInstrumentsAll:
-                mark = pytest.mark.parametrize("name", instruments['names'])
-                getattr(InstClass, method).pytestmark.append(mark)
-            elif InstClass == TestInstrumentsDownload:
-                mark = pytest.mark.parametrize("inst", instruments['download'])
-                getattr(InstClass, method).pytestmark.append(mark)
-            elif InstClass == TestInstrumentsNoDownload:
-                mark = pytest.mark.parametrize("inst",
-                                               instruments['no_download'])
-                getattr(InstClass, method).pytestmark.append(mark)
+method_list = [func for func in dir(InstTestClass)
+               if callable(getattr(InstTestClass, func))]
+# Search tests for iteration via pytestmark, update instrument list
+for method in method_list:
+    if hasattr(getattr(InstTestClass, method), 'pytestmark'):
+        # Get list of names of pytestmarks
+        Nargs = len(getattr(InstTestClass, method).pytestmark)
+        names = [getattr(InstTestClass, method).pytestmark[j].name
+                 for j in range(0, Nargs)]
+        # Add instruments from your library
+        if 'all_inst' in names:
+            mark = pytest.mark.parametrize("name", instruments['names'])
+            getattr(InstTestClass, method).pytestmark.append(mark)
+        elif 'download' in names:
+            mark = pytest.mark.parametrize("inst", instruments['download'])
+            getattr(InstTestClass, method).pytestmark.append(mark)
+        elif 'no_download' in names:
+            mark = pytest.mark.parametrize("inst", instruments['no_download'])
+            getattr(InstTestClass, method).pytestmark.append(mark)
 
 
-class TestModels(TestInstrumentsAll):
-
-    __test__ = True
-
-    def setup(self):
-        """Runs before every method to create a clean testing setup."""
-        self.package = 'pysatModels.models'
-        pass
-
-    def teardown(self):
-        """Runs after every method to clean up previous testing."""
-        pass
-
-
-class TestModelsDownload(TestInstrumentsDownload):
-
-    __test__ = True
-
-    def setup(self):
-        """Runs before every method to create a clean testing setup."""
-        self.package = 'pysatModels.models'
-        pass
-
-    def teardown(self):
-        """Runs after every method to clean up previous testing."""
-        pass
-
-
-class TestModelsNoDownload(TestInstrumentsNoDownload):
-
-    __test__ = True
+class TestModels(InstTestClass):
 
     def setup(self):
         """Runs before every method to create a clean testing setup."""
