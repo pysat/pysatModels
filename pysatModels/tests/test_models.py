6b2b341048b91a10219dc870b63a5a576636161b
Angeline Burrell <angeline.burrell@nrl.navy.mil>
2020-09-21 15:30:27 -0400

    DEP: updated testing methods
    Updated the pysat Instrument testing methods.

--- pysatModels/tests/test_models.py
+++ pysatModels/tests/test_models.py
@@ -1,38 +1,43 @@
 import pytest
 
 import pysatModels
-from pysat.tests.instrument_test_class import generate_instrument_list
+from pysat.utils import generate_instrument_list
 from pysat.tests.instrument_test_class import InstTestClass
 
-instruments = generate_instrument_list(package=pysatModels.models)
+instruments = generate_instrument_list(inst_loc=pysatModels.models)
 
 method_list = [func for func in dir(InstTestClass)
                if callable(getattr(InstTestClass, func))]
+
 # Search tests for iteration via pytestmark, update instrument list
 for method in method_list:
     if hasattr(getattr(InstTestClass, method), 'pytestmark'):
         # Get list of names of pytestmarks
-        Nargs = len(getattr(InstTestClass, method).pytestmark)
-        names = [getattr(InstTestClass, method).pytestmark[j].name
-                 for j in range(0, Nargs)]
+        mark_name = [mod_mark.name for mod_mark
+                     in getattr(InstTestClass, method).pytestmark]
+
         # Add instruments from your library
-        if 'all_inst' in names:
-            mark = pytest.mark.parametrize("name", instruments['names'])
+        if 'all_inst' in mark_name:
+            mark = pytest.mark.parametrize("inst_name", instruments['names'])
             getattr(InstTestClass, method).pytestmark.append(mark)
-        elif 'download' in names:
-            mark = pytest.mark.parametrize("inst", instruments['download'])
+        elif 'download' in mark_name:
+            mark = pytest.mark.parametrize("inst_dict",
+                                           instruments['download'])
             getattr(InstTestClass, method).pytestmark.append(mark)
-        elif 'no_download' in names:
-            mark = pytest.mark.parametrize("inst", instruments['no_download'])
+        elif 'no_download' in mark_name:
+            mark = pytest.mark.parametrize("inst_dict",
+                                           instruments['no_download'])
             getattr(InstTestClass, method).pytestmark.append(mark)
 
 
 class TestModels(InstTestClass):
 
     def setup(self):
-        """Runs before every method to create a clean testing setup."""
-        self.package = pysatModels.models
+        """Runs before every method to create a clean testing setup
+        """
+        self.inst_loc = pysatModels.models
 
     def teardown(self):
-        """Runs after every method to clean up previous testing."""
-        del self.package
+        """Runs after every method to clean up previous testing
+        """
+        del self.inst_loc
