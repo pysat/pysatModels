965f6ff61d4b2148918aae1a2c72c8c27ecfc635
Angeline Burrell <angeline.burrell@nrl.navy.mil>
2021-04-21 10:20:00 -0400

    STY: updated doc config script
    Updated the documentation configuration script to conform to PEP8 standards.

--- docs/conf.py
+++ docs/conf.py
@@ -38,8 +38,7 @@ extensions = ['sphinx.ext.autodoc',
               'sphinx.ext.viewcode',
               'sphinx.ext.githubpages',
               'numpydoc',
-              'm2r',
-]
+              'm2r']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['.templates']
@@ -55,7 +54,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'pysatModels'
-copyright = u'2020, Angeline G Burrell, Russell Stoneback, Jeffery Klenzing'
+cr_line = u'2020, Angeline G Burrell, Russell Stoneback, Jeffery Klenzing'
 author = u'Angeline G Burrell, Russell Stoneback, Jeffery Klenzing'
 
 # The version info for the project you're documenting, acts as replacement for
@@ -64,6 +63,7 @@ author = u'Angeline G Burrell, Russell Stoneback, Jeffery Klenzing'
 #
 # The short X.Y version.
 version = u'0.1.0'
+
 # The full version, including alpha/beta/rc tags.
 release = u'0.1.0-alpha'
 
@@ -102,7 +102,8 @@ html_theme = 'alabaster'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-#html_static_path = ['.static']
+#
+# html_static_path = ['.static']
 
 
 # -- Options for HTMLHelp output ------------------------------------------
@@ -112,56 +113,43 @@ htmlhelp_basename = 'pysatModelsdoc'
 
 
 # -- Options for LaTeX output ---------------------------------------------
-
-latex_elements = {
-    # The paper size ('letterpaper' or 'a4paper').
-    #
-    # 'papersize': 'letterpaper',
-
-    # The font size ('10pt', '11pt' or '12pt').
-    #
-    # 'pointsize': '10pt',
-
-    # Additional stuff for the LaTeX preamble.
-    #
-    # 'preamble': '',
-
-    # Latex figure (float) alignment
-    #
-    # 'figure_align': 'htbp',
-}
+#
+# These options may be included in the dictionary below:
+#   The paper size ('letterpaper' or 'a4paper').
+#   'papersize': 'letterpaper',
+#
+#   The font size ('10pt', '11pt' or '12pt').
+#   'pointsize': '10pt',
+#
+#   Additional stuff for the LaTeX preamble.
+#   'preamble': '',
+#
+#   Latex figure (float) alignment
+#   'figure_align': 'htbp',
+latex_elements = {}
 
 # Grouping the document tree into LaTeX files. List of tuples
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
-latex_documents = [
-    (master_doc, 'pysatModels.tex', u'pysatModels Documentation',
-     u'Angeline G Burrell, Russel Stoneback, and Jeffery Klenzing', 'manual'),
-]
+latex_documents = [(master_doc, 'pysatModels.tex', u'pysatModels Documentation',
+                    author, 'manual')]
 
 
 # -- Options for manual page output ---------------------------------------
 
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
-man_pages = [
-    (master_doc, 'pysatModels', u'pysatModels Documentation',
-     [author], 1)
-]
-
+man_pages = [(master_doc, project, u'pysatModels Documentation',
+              [author], 1)]
 
 # -- Options for Texinfo output -------------------------------------------
 
 # Grouping the document tree into Texinfo files. List of tuples
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
-texinfo_documents = [
-    (master_doc, 'pysatModels', u'pysatModels Documentation',
-     author, 'pysatModels', 'Tools for Model-Data analysis.',
-     'Space Physics'),
-]
-
-
+texinfo_documents = [(master_doc, project, u'pysatModels Documentation',
+                      author, project, 'Tools for Model-Data analysis.',
+                      'Space Physics')]
 
 # -- Options for Epub output ----------------------------------------------
 
@@ -169,7 +157,7 @@ texinfo_documents = [
 epub_title = project
 epub_author = author
 epub_publisher = author
-epub_copyright = copyright
+epub_copyright = cr_line
 
 # The unique identifier of the text. This can be a ISBN number
 # or the project homepage.
@@ -182,8 +170,6 @@ epub_copyright = copyright
 
 # A list of files that should not be packed into the epub file.
 epub_exclude_files = ['search.html']
-
-
 
 # Example configuration for intersphinx: refer to the Python standard library.
 intersphinx_mapping = {'https://docs.python.org/': None}
