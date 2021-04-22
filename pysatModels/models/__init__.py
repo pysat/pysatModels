9d1f4f38bc22fe024dbf8a0d72a1445ba877bf06
Angeline Burrell <angeline.burrell@nrl.navy.mil>
2021-04-22 10:00:12 -0400

    BUG: added pydineofs import
    Added the missing pydineofs import.  Also removed support for python 2.

--- pysatModels/models/__init__.py
+++ pysatModels/models/__init__.py
@@ -11,11 +11,8 @@ Routines for loading model data into a pysat Instrument object
 
 """
 
-from __future__ import absolute_import
-from __future__ import unicode_literals
-
 # Import key modules and skip F401 testing in flake8
-from pysatModels.models import sami2py_sami2  # noqa: F401
+from pysatModels.models import pydineof_dineof, sami2py_sami2  # noqa: F401
 from pysatModels.models import ucar_tiegcm  # noqa: F401
 
 __all__ = ['pydineof_dineof', 'sami2py_sami2', 'ucar_tiegcm']
