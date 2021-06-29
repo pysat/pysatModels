43f9325950a85dd9f321e43d746fabf9e9d9abc3
Angeline Burrell <angeline.burrell@nrl.navy.mil>
2021-06-29 11:06:55 -0400

    ENH: updated utils init
    Updated the utils init file by:
    - removing python 2 lines,
    - updating docstring and copyright to current standards, and
    - adding new sub-module import.

--- pysatModels/utils/__init__.py
+++ pysatModels/utils/__init__.py
@@ -1,19 +1,15 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# Copyright (C) 2019, AGB & pysat team
+# Copyright (C) 2019, pysat team
 # Full license can be found in License.md
 # -----------------------------------------------------------------------------
 """
-pysatModels.utils
-=================
-
 Utilities designed to extract, match, and compare modelled and observed data
 
 """
-
-from __future__ import absolute_import, unicode_literals
 
 # Import key modules and skip F401 testing in flake8
 from pysatModels.utils import extract  # noqa: F401
 from pysatModels.utils import match  # noqa: F401
 from pysatModels.utils import compare  # noqa: F401
+from pysatModels.utils import convert  # noqa: F401
