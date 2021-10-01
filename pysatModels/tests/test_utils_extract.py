517c092de11879059dc9c40cd320c1ac378b0f6e
Russell Stoneback <rstoneba@utdallas.edu>
2021-10-01 15:36:07 -0500

    TST: Added test for altitude translation tolerance.

--- pysatModels/tests/test_utils_extract.py
+++ pysatModels/tests/test_utils_extract.py
@@ -357,3 +357,166 @@ class TestUtilsExtractInstModView(object):
                 assert len(self.inst.data[tcol][
                     ~np.isnan(self.inst.data[tcol])]) > 0
         return
+
+class TestUtilsAltitudePressure(object):
+    """Unit tests for `utils.extract.instrument_altitude_to_model_pressure`."""
+
+    def setup(self):
+        """Set up the unit test environment for each method."""
+
+        self.inst = pysat.Instrument(platform='pysat', name='testing')
+        self.model = pysat.Instrument(inst_module=pysat_testmodel,
+                                      tag='pressure_levels')
+        self.inst.load(date=pysat_testmodel._test_dates[''][''])
+        self.model.load(date=pysat_testmodel._test_dates[''][''])
+
+        self.input_args = [self.inst, self.model.data,
+                           ["altitude",  "latitude", "longitude"],
+                           ["ilev", "latitude", "longitude"],
+                           "time", "time", ['', "deg", "deg"],
+                           'altitude', 'altitude', 'cm']
+        self.input_kwargs = {}
+
+        self.out = []
+        self.log_capture = StringIO()
+        ps_mod.logger.addHandler(logging.StreamHandler(self.log_capture))
+        ps_mod.logger.setLevel(logging.INFO)
+        return
+
+    def teardown(self):
+        """Clean up the unit test environment after each method."""
+
+        del self.inst, self.model, self.input_args, self.out
+        del self.input_kwargs, self.log_capture
+        return
+
+    @pytest.mark.parametrize("bad_index,bad_input,err_msg",
+                             [(2, [], "Must provide inst_name as a list"),
+                              (2, [''], 'Must provide the same number of'),
+                              (2, ['glon', 'latitude', 'altitude'],
+                               "Unknown instrument location index"),
+                              (3, [], "Must provide mod_name as a list"),
+                              (3, [''], "Must provide the same number"),
+                              (6, [], "Must provide units for each "),
+                              (4, "naname", "unknown model name for datetime"),
+                              (5, "naname", "Unknown model time coordinate"),
+                              (7, 'navar', 'Unknown Instrument altitude key'),
+                              (8, 'navar', 'Unknown Model altitude key')])
+    def test_bad_arg_input(self, bad_index, bad_input, err_msg):
+        """Test for expected failure with bad input arguments."""
+
+        self.input_args[bad_index] = bad_input
+
+        with pytest.raises(ValueError) as verr:
+            extract.instrument_altitude_to_model_pressure(*self.input_args)
+
+        assert str(verr.value.args[0]).find(err_msg) >= 0
+        return
+
+    # @pytest.mark.parametrize("bad_key,bad_val,err_msg",
+    #                          [("sel_name", ["altitude"],
+    #                            "No model data keys to interpolate"),
+    #                           ("method", "not_a_method",
+    #                            "interpn only understands the methods"),
+    #                           ("model_label", 1, "Unknown format code "),
+    #                           ("time_method", "fun", "unknown time method"),
+    #                           ("pair_method", "fun",
+    #                            "unknown pairing method")])
+    # def test_bad_kwarg_input(self, bad_key, bad_val, err_msg):
+    #     """Test for expected failure with bad kwarg input."""
+    #
+    #     self.input_kwargs[bad_key] = bad_val
+    #
+    #     with pytest.raises(Exception) as err:
+    #         extract.extract_modelled_observations(*self.input_args,
+    #                                               **self.input_kwargs)
+    #
+    #     assert str(err.value.args[0]).find(err_msg) >= 0
+    #     return
+    #
+    @pytest.mark.parametrize("tol_val", [10., 1., 0.1])
+    def test_good_translation(self, tol_val):
+        """Test for success with different altitude tolerances"""
+
+        self.input_kwargs = {"tol": tol_val}
+        self.out = extract.instrument_altitude_to_model_pressure(
+            *self.input_args,
+            **self.input_kwargs)
+
+        # Calculate difference in altitude (Instrument and values extracted
+        # from Model) and ensure it is less than specified tolerance.
+        print('Inst altitude ', self.inst['altitude'])
+        print('Returned Inteproalted alt ', self.out[0])
+        alt_diff = np.abs(self.inst[self.out[0]] - self.inst['altitude'])
+        assert np.all(alt_diff <= tol_val)
+        return
+    #
+    # def test_success_w_out_of_bounds(self):
+    #     """Test extraction success for all variables without UT dependence."""
+    #
+    #     self.input_kwargs["model_label"] = self.model_label
+    #     self.out = extract.extract_modelled_observations(*self.input_args,
+    #                                                      **self.input_kwargs)
+    #     lout = self.log_capture.getvalue()
+    #
+    #     assert lout.find('One of the requested xi is out of bounds') >= 0
+    #
+    #     for label in self.input_kwargs['sel_name']:
+    #         if label not in self.input_args[3]:
+    #             # Test each of the extracted model data columns
+    #             tcol = "{:s}_{:s}".format(self.model_label, label)
+    #             assert tcol in self.out
+    #             assert tcol in self.inst.data.columns
+    #             assert (self.inst.data[self.input_args[2][0]].shape
+    #                     == self.inst.data[tcol].shape)
+    #             assert len(self.inst.data[tcol][
+    #                 ~np.isnan(self.inst.data[tcol])]) > 0
+    #     return
+    #
+    # def test_failure_for_already_ran_data(self):
+    #     """Test the failure for all model variables already extracted."""
+    #
+    #     self.input_kwargs["model_label"] = self.model_label
+    #
+    #     # Run everything successfully once
+    #     extract.extract_modelled_observations(*self.input_args,
+    #                                           **self.input_kwargs)
+    #
+    #     # Run everything again, raising a value error
+    #     with pytest.raises(ValueError) as err:
+    #         extract.extract_modelled_observations(*self.input_args,
+    #                                               **self.input_kwargs)
+    #
+    #         self.out = self.log_capture.getvalue()
+    #         assert self.out.find('model data already interpolated') >= 0
+    #
+    #     assert str(err.value.args[0]).find(
+    #         'instrument object already contains all model data') >= 0
+    #     return
+    #
+    # def test_success_for_some_already_ran_data(self):
+    #     """Test the success for some model variables already extracted."""
+    #
+    #     all_sel = list(self.input_kwargs['sel_name'])
+    #     self.input_kwargs['model_label'] = self.model_label
+    #
+    #     # Run through twice
+    #     for i, selected in enumerate([all_sel[2:], all_sel]):
+    #         self.input_kwargs['sel_name'] = selected
+    #         self.out = extract.extract_modelled_observations(
+    #             *self.input_args, **self.input_kwargs)
+    #         lout = self.log_capture.getvalue()
+    #
+    #     assert lout.find('model data already interpolated') >= 0
+    #
+    #     for label in all_sel:
+    #         if label not in self.input_args[3]:
+    #             # Test each of the extracted model data columns
+    #             tcol = "{:s}_{:s}".format(self.model_label, label)
+    #             assert tcol in self.inst.data.columns
+    #             assert (self.inst.data[self.input_args[2][0]].shape
+    #                     == self.inst.data[tcol].shape)
+    #             assert len(self.inst.data[tcol][
+    #                 ~np.isnan(self.inst.data[tcol])]) > 0
+    #
+    #     return
