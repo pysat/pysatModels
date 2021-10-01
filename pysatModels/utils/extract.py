8354f439934bb4136c2564f28dccad460f0cc029
Russell Stoneback <rstoneba@utdallas.edu>
2021-10-01 17:14:12 -0500

    STY: Updated returns docstring info

--- pysatModels/utils/extract.py
+++ pysatModels/utils/extract.py
@@ -68,8 +68,8 @@ def instrument_altitude_to_model_pressure(inst, model, inst_name, mod_name,
 
     Returns
     -------
-    interp_data.keys() : Keys
-        Keys of modelled data added to the instrument (inst_out)
+    inst_out_alt, inst_out_pres
+        Keys of modelled data added to the instrument
 
 
     Notes
