de34ce2c341738a740ca8305ff49f904c1c38b30
Angeline Burrell <angeline.burrell@nrl.navy.mil>
2022-02-24 09:35:24 -0500

    STY: updated logger name
    Updated logger name to not look like a pysat sub-module.

--- pysatModels/__init__.py
+++ pysatModels/__init__.py
@@ -26,4 +26,4 @@ del local_dir, version_file
 
 # Define a logger object to allow easier log handling
 logging.raiseExceptions = False
-logger = logging.getLogger('pysat.pysatModels')
+logger = logging.getLogger('pysatModels')
