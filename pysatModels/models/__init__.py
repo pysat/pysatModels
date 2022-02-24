f3f2521aa395ec027ac2ef6700adb668015787a2
Angeline Burrell <angeline.burrell@nrl.navy.mil>
2022-02-24 09:35:07 -0500

    BUG: added missing import
    Added the missing methods import.

--- pysatModels/models/__init__.py
+++ pysatModels/models/__init__.py
@@ -6,6 +6,7 @@
 """Routines for loading model data into a pysat Instrument object."""
 
 # Import key modules and skip F401 testing in flake8
+from pysatModels.models import methods  #noqa: F401
 from pysatModels.models import pydineof_dineof  # noqa: F401
 from pysatModels.models import sami2py_sami2  # noqa: F401
 from pysatModels.models import ucar_tiegcm  # noqa: F401
