342faa8f58d8fee0789fb9d6e7d9a25584dbdf3b
Russell Stoneback <rstoneba@utdallas.edu>
2022-04-18 16:34:03 -0500

    ENH: Updated if/else check. Expanded comments.

--- pysatModels/models/sami2py_sami2.py
+++ pysatModels/models/sami2py_sami2.py
@@ -23,6 +23,7 @@ inst_id
 
 import datetime as dt
 import functools
+import pack_version
 import warnings
 
 import pysat
@@ -136,11 +137,15 @@ def load(fnames, tag=None, inst_id=None, **kwargs):
     data, meta = pysat.utils.load_netcdf4(fnames, pandas_format=False,
                                           epoch_name='ut')
 
-    # Add time variable for pysat compatibility
-    if 'time' in data:
+    # Add time variable for pysat compatibility. For pysat <= v3.0.1, time
+    # from sami files will be loaded as 'ut'. For pysat >= v3.0.2, support
+    # for `epoch_name` and xarray results in the disk variable 'ut' being named
+    # 'time' when returned from `load_netcdf4` above.
+    if pack_version.Version(pysat.__version__) >= pack_version.Version('3.0.2'):
         label = 'time'
-    elif 'ut' in data:
+    else:
         label = 'ut'
+
     data['time'] = [dt.datetime(2019, 1, 1)
                     + dt.timedelta(seconds=int(val * 3600.0))
                     for val in data[label].values]
