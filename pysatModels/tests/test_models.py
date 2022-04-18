915aaf6680803f708be603d95c5b77e7c42acd52
Russell Stoneback <rstoneba@utdallas.edu>
2022-04-18 17:49:33 -0500

    TST: Added test for sami compatibility across pysat versions.

--- pysatModels/tests/test_models.py
+++ pysatModels/tests/test_models.py
@@ -6,6 +6,7 @@ Imports test methods from pysat.tests.instrument_test_class
 
 """
 
+from packaging import version as pack_version
 import pytest
 import tempfile
 
@@ -69,4 +70,63 @@ class TestModels(InstTestClass):
         pysat.params.data['data_dirs'] = self.saved_path
         self.tempdir.cleanup()
         del self.inst_loc, self.saved_path, self.tempdir
+        return
+
+
+class TestSAMIPysatVersion(object):
+    """Test SAMI load code for pysat version differences across v3.0.2"""
+
+    def setup_class(self):
+        """Initialize the testing setup once before all tests are run."""
+        # Make sure to use a temporary directory so that the user setup is not
+        # altered
+        self.tempdir = tempfile.TemporaryDirectory()
+        self.saved_path = pysat.params['data_dirs']
+        self.saved_ver = pysat.__version__
+        pysat.params.data['data_dirs'] = [self.tempdir.name]
+
+        # Assign the location of the model Instrument sub-modules
+        self.inst_loc = pysatModels.models
+        return
+
+    def teardown_class(self):
+        """Clean up downloaded files and parameters from tests."""
+
+        pysat.params.data['data_dirs'] = self.saved_path
+        pysat.__version__ = self.saved_ver
+
+        self.tempdir.cleanup()
+        del self.inst_loc, self.saved_path, self.tempdir, self.saved_ver
+        return
+
+    def test_against_version(self):
+        """Test for SAMI load failure for pysat version opposite v3.0.2.
+        """
+
+        if (pack_version.Version(pysat.__version__)
+                >= pack_version.Version('3.0.2')):
+            # Define target error variable label
+            label = 'ut'
+
+            # Replace reported version with one before 3.0.2
+            vlabel = '3.0.1'
+        else:
+            # Define target error variable label
+            label = 'time'
+
+            # Replace reported version with one including 3.0.2
+            vlabel = '3.0.2'
+
+        # Update reported pysat version
+        pysat.__version__ = vlabel
+
+        with pytest.raises(KeyError) as verr:
+            inst = pysat.Instrument(inst_module=self.inst_loc.sami2py_sami2,
+                                    tag='test')
+            inst.download(self.inst_loc.sami2py_sami2._test_dates['']['test'],
+                          self.inst_loc.sami2py_sami2._test_dates['']['test'])
+            inst.load(date=self.inst_loc.sami2py_sami2._test_dates['']['test'])
+
+        assert str(verr).find(label) >= 0
+
         return
