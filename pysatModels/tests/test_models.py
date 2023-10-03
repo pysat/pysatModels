a7c186e9a0c6812aab75d592ec188a83a4d7a09a
Jeff Klenzing <jklenzing@users.noreply.github.com>
2023-10-03 17:29:59 -0400

    MAINT: update intrument test class

--- pysatModels/tests/test_models.py
+++ pysatModels/tests/test_models.py
@@ -14,77 +14,26 @@ import shutil
 import sys
 import tempfile
 
+# Import the test classes from pysat
 import pysat
-from pysat.tests.instrument_test_class import InstTestClass
+from pysat.tests.classes import cls_instrument_library as clslib
 
 import pysatModels
 
 # Retrieve the lists of Model instruments and testing methods
-instruments = pysat.utils.generate_instrument_list(inst_loc=pysatModels.models)
-method_list = [func for func in dir(InstTestClass)
-               if callable(getattr(InstTestClass, func))]
-
-# Search tests for iteration via pytestmark, update instrument list
-for method in method_list:
-    if hasattr(getattr(InstTestClass, method), 'pytestmark'):
-        # Get list of names of pytestmarks
-        mark_name = [mod_mark.name for mod_mark
-                     in getattr(InstTestClass, method).pytestmark]
-
-        # Add instruments from your library
-        if 'all_inst' in mark_name:
-            mark = pytest.mark.parametrize("inst_name", instruments['names'])
-            getattr(InstTestClass, method).pytestmark.append(mark)
-        elif 'download' in mark_name:
-            mark = pytest.mark.parametrize("inst_dict",
-                                           instruments['download'])
-            getattr(InstTestClass, method).pytestmark.append(mark)
-        elif 'no_download' in mark_name:
-            mark = pytest.mark.parametrize("inst_dict",
-                                           instruments['no_download'])
-            getattr(InstTestClass, method).pytestmark.append(mark)
+instruments = clslib.InstLibTests.initialize_test_package(
+    clslib.InstLibTests, inst_loc=pysatModels.models)
 
 
-class TestModels(InstTestClass):
+class TestModels(clslib.InstLibTests):
     """Main class for instrument tests.
 
     Note
     ----
-    Uses class level setup and teardown so that all tests use the same
-    temporary directory. We do not want to geneate a new tempdir for each test,
-    as the load tests need to be the same as the download tests.
+    All standard tests, setup, and teardown inherited from the core pysat
+    instrument test class.
 
     """
-
-    def setup_class(self):
-        """Initialize the testing setup once before all tests are run."""
-        # Make sure to use a temporary directory so that the user setup is not
-        # altered
-        # TODO(#100): remove if-statement when it is always triggered
-        tkwargs = {}
-        if sys.version_info.major >= 3 and sys.version_info.minor >= 10:
-            tkwargs = {"ignore_cleanup_errors": True}
-        self.tempdir = tempfile.TemporaryDirectory(**tkwargs)
-        self.saved_path = pysat.params['data_dirs']
-        pysat.params.data['data_dirs'] = [self.tempdir.name]
-
-        # Assign the location of the model Instrument sub-modules
-        self.inst_loc = pysatModels.models
-        return
-
-    def teardown_class(self):
-        """Clean up downloaded files and parameters from tests."""
-
-        pysat.params.data['data_dirs'] = self.saved_path
-
-        # TODO(#100): Remove try/except when Python 3.10 is the lowest version
-        try:
-            self.tempdir.cleanup()
-        except Exception:
-            pass
-
-        del self.inst_loc, self.saved_path, self.tempdir
-        return
 
 
 class TestSAMIPysatVersion(object):
